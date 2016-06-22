370f39f3ccfd0a337af5a58b7b2ae7433dfcb478
Niko Caron <niko.caron90@gmail.com>
2016-06-22 02:33:45 -0300

    Results Array
    Adds filtered search results to an array.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 var search = function() {
 	var input = $('#input').val();
 	var searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info%7Cextracts&generator=search&redirects=1&callback=&utf8=1&inprop=url&exsentences=3&exlimit=10&exintro=1&explaintext=1&gsrsearch=' + input;
+	var resultsArray = [];
 	$('#input').val('');
 
 	// Gets the search results from Wikipedia
+	// Adds filtered results to an array
 	$.ajax({
 		url: searchUrl,
 		dataType: 'jsonp'
 	})
 	.done(function(data) {
-		console.log(data);
+		$.each(data.query.pages, function(index, value) {
+			if (value.extract.toLowerCase().indexOf('may refer to:') < 0) {
+				resultsArray.push({
+					'title': value.title,
+					'extract': value.extract,
+					'url': value.canonicalurl
+				});
+			}
+		});
 	});
 };
 

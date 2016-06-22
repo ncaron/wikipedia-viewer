786cf2a488eae3c982c7a2c0f5d476278072f387
Niko Caron <niko.caron90@gmail.com>
2016-06-22 02:12:11 -0300

    URL Change
    Changed the Wikipedia URL to include more information in the request.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var search = function() {
 	var input = $('#input').val();
-	var searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&callback=test&srprop=snippet&srlimit=10&srsearch=' + input;
+	var searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info%7Cextracts&generator=search&redirects=1&callback=&utf8=1&inprop=url&exsentences=3&exlimit=10&exintro=1&explaintext=1&gsrsearch=' + input;
 	$('#input').val('');
 
 	// Gets the search results from Wikipedia

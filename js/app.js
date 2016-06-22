e18281b7a1e4afa181b2f0c020199755d0530694
Niko Caron <niko.caron90@gmail.com>
2016-06-21 23:06:57 -0300

    Wikipedia Search
    Gets the data from Wikipedia using the user's search input and logs it to the console.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 var search = function() {
 	var input = $('#input').val();
+	var searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&callback=test&srprop=snippet&srlimit=10&srsearch=' + input;
 	$('#input').val('');
+
+	// Gets the search results from Wikipedia
+	$.ajax({
+		url: searchUrl,
+		dataType: 'jsonp'
+	})
+	.done(function(data) {
+		console.log(data);
+	});
 };
 
+// Triggers the search function when 'enter' key is pressed
 $('#input').keyup(function() {
 	if (event.keyCode == 13) {
 		$('#submit').trigger('click');
